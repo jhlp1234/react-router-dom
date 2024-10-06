ca7fb9e93fb5a916a6a0403bc32cd38f60d3a0fd
jhlp <t01087912432@gmail.com>
2024-10-06 22:54:35 +0900

    react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function Test() {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home/>}></Route>
+        <Route path='/test' element={<Test/>}></Route>
+      </Routes>
+      <li><Link to="/">Home</Link></li>
+      <li><a href='/test'>Test</a></li>
+      </BrowserRouter>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
